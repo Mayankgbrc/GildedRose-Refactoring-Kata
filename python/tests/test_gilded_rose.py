7bbfd1890b2ac6efb58b7e6a95eaedd50bb734f0
Mayank Gupta <mayankgbrc@gmail.com>
2026-01-02 13:46:22 +0530

    Added test cases

--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -5,11 +5,48 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+    def test_sulfuras(self):
+        item = Item("Sulfuras, Hand of Ragnaros", 5, 80)
+        gilded_rose = GildedRose([item])
+        for _ in range(2):
+            gilded_rose.update_quality()
+        self.assertEqual(80, item.quality)
+        self.assertEqual(5, item.sell_in)
+
+    def test_aged_brie(self):
+        item = Item("Aged Brie",3, 10)
+        gilded_rose = GildedRose([item])
+        for _ in range(5):
+            gilded_rose.update_quality()
+        self.assertEqual(17, item.quality) 
+    
+    def test_aged_brie_2(self):
+        item = Item("Aged Brie",8, 10)
+        gilded_rose = GildedRose([item])
+        for _ in range(4):
+            gilded_rose.update_quality()
+        self.assertEqual(14, item.quality) 
+    
+    def test_backstage_1(self):
+        item = Item("Backstage passes to a TAFKAL80ETC concert",7, 25)
+        gilded_rose = GildedRose([item])
+        for _ in range(5):
+            gilded_rose.update_quality()
+        self.assertEqual(38, item.quality) 
+    
+    def test_backstage_2(self):
+        item = Item("Backstage passes to a TAFKAL80ETC concert",7, 25)
+        gilded_rose = GildedRose([item])
+        for _ in range(10):
+            gilded_rose.update_quality()
+        self.assertEqual(0, item.quality) 
+
+    def test_general(self):
+        item = Item("Star Wars",3, 25)
+        gilded_rose = GildedRose([item])
+        for _ in range(5):
+            gilded_rose.update_quality()
+        self.assertEqual(18, item.quality) 
 
         
 if __name__ == '__main__':
